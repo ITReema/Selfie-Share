97e2cc08daf4dfe2f7a628f8c47faf53331e17a0
Reema <it.reema7@gmail.com>
2019-06-11 05:01:37 +0300

    Add files via upload

---
+++ Selfie Share/Selfie Share/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Selfie Share
+//
+//  Created by mac_os on 08/10/1440 AH.
+//  Copyright © 1440 mac_os. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    var images = [UIImage]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Selfie Share"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
+    }
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        dismiss(animated: true)
+        
+        images.insert(image, at: 0)
+        collectionView.reloadData()
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageView", for: indexPath)
+        
+        if let imageView = cell.viewWithTag(1000) as? UIImageView {
+            imageView.image = images[indexPath.item]
+        }
+        
+        return cell
+    }
+
+
+}
+
